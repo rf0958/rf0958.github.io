5ddefc12a32941c02234b89c27d597528d48b674
ireallylikecoding5 <rf4394829@gmail.com>
2025-03-23 12:00:47 -0700

    Add files via upload

--- Application/script.js
+++ Application/script.js
@@ -1,6 +1,6 @@
 function jbh98753(){
     console.log("Amazing!")
     console.log("This is a special game that won't go live for a few months:")
-    console.log("<this domain>/Application/3DEarth")
+    console.log("https://rf0958.github.io/Application/3dEarth/Advanced")
     console.log("Thx. DONT TELL UR FRIENDS lol")
 }
