4168b380b4e1c302e96b1efef983c076fe522e76
ireallylikecoding5 <rf4394829@gmail.com>
2025-03-22 20:00:04 -0700

    Add files via upload

---
+++ Application/script.js
@@ -0,0 +1,6 @@
+function jbh98753(){
+    console.log("Amazing!")
+    console.log("This is a special game that won't go live for a few months:")
+    console.log("<this domain>/Application/3DEarth")
+    console.log("Thx. DONT TELL UR FRIENDS lol")
+}
